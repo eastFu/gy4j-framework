a6878c6d81cabf769043538546b687715a0eff0c
eastFu <492510931@qq.com>
2017-09-27 23:51:58 +0800

    update

---
+++ src/main/java/cn/gyyx/gy4j/listener/WebContextListener.java
@@ -0,0 +1,9 @@
+package cn.gyyx.gy4j.listener;
+
+import org.springframework.web.context.ContextLoaderListener;
+
+/**
+ * Created by Administrator on 2017/9/27 0027.
+ */
+public class WebContextListener extends ContextLoaderListener {
+}
