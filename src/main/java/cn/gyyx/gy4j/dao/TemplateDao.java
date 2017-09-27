a956a8f566ad0004c5623fadcf07ca5cc223e75b
eastFu <492510931@qq.com>
2017-09-27 23:42:51 +0800

    update

---
+++ src/main/java/cn/gyyx/gy4j/dao/TemplateDao.java
@@ -0,0 +1,8 @@
+package cn.gyyx.gy4j.dao;
+
+/**
+ * Created by Administrator on 2017/9/27 0027.
+ */
+public abstract class TemplateDao<T> implements BaseMapper<T>{
+    
+}
---
+++ src/main/java/cn/gyyx/gy4j/model/BaseModel.java
@@ -0,0 +1,7 @@
+package cn.gyyx.gy4j.model;
+
+/**
+ * Created by Administrator on 2017/9/27 0027.
+ */
+public interface BaseModel {
+}
