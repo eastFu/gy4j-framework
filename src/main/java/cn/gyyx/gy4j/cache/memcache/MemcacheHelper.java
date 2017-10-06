93c6a99830644a3c11f974985ca341b8ef138b36
eastFu <492510931@qq.com>
2017-10-06 21:35:22 +0800

    0.0.1

---
+++ src/main/java/cn/gyyx/gy4j/cache/memcache/MemcacheHelper.java
@@ -0,0 +1,9 @@
+package cn.gyyx.gy4j.cache.memcache;
+
+/**
+ * @Author : east.Fu
+ * @Description :
+ * @Date : Created in  2017/10/6 21:33
+ */
+public class MemcacheHelper {
+}
---
+++ src/main/java/cn/gyyx/gy4j/cache/mogodb/MogodbHelper.java
@@ -0,0 +1,9 @@
+package cn.gyyx.gy4j.cache.mogodb;
+
+/**
+ * @Author : east.Fu
+ * @Description :
+ * @Date : Created in  2017/10/6 21:33
+ */
+public class MogodbHelper {
+}
