714211018dc94eeb9480d1b675920ec12c44aabc
eastFu <492510931@qq.com>
2017-10-07 21:08:07 +0800

    0.0.1

--- src/main/java/cn/gyyx/gy4j/cache/mogodb/MogodbHelper.java
+++ src/main/java/cn/gyyx/gy4j/cache/mogodb/MogodbHelper.java
@@ -1,9 +1,14 @@
 package cn.gyyx.gy4j.cache.mogodb;
 
+import org.apache.log4j.Logger;
+
 /**
  * @Author : east.Fu
  * @Description :
  * @Date : Created in  2017/10/6 21:33
  */
 public class MogodbHelper {
+
+    private static final Logger LOG = Logger.getLogger(MogodbHelper.class);
+
 }
--- src/main/java/cn/gyyx/gy4j/cache/redis/RedisHelper.java
+++ src/main/java/cn/gyyx/gy4j/cache/redis/RedisHelper.java
@@ -1,9 +1,13 @@
 package cn.gyyx.gy4j.cache.redis;
 
+import org.apache.log4j.Logger;
+
 /**
  * @Author : east.Fu
  * @Description :
  * @Date : Created in  2017/10/5 21:54
  */
 public class RedisHelper {
+
+    private static final Logger LOG = Logger.getLogger(RedisHelper.class);
 }
