56d8a60180c6289d15d38a6c6f27ec8d469311b8
eastFu <492510931@qq.com>
2017-10-07 21:08:33 +0800

    0.0.1

--- src/main/java/cn/gyyx/gy4j/cache/CacheHelper.java
+++ src/main/java/cn/gyyx/gy4j/cache/CacheHelper.java
@@ -1,10 +1,14 @@
 package cn.gyyx.gy4j.cache;
 
+
+import org.apache.log4j.Logger;
+
 /**
  * @Author : east.Fu
  * @Description :
  * @Date : Created in  2017/10/6 21:34
  */
 public class CacheHelper {
-    
+
+    private static final Logger LOG = Logger.getLogger(CacheHelper.class);
 }
--- src/main/java/cn/gyyx/gy4j/cache/memcache/MemcacheHelper.java
+++ src/main/java/cn/gyyx/gy4j/cache/memcache/MemcacheHelper.java
@@ -1,9 +1,14 @@
 package cn.gyyx.gy4j.cache.memcache;
 
+import org.apache.log4j.Logger;
+
 /**
  * @Author : east.Fu
  * @Description :
  * @Date : Created in  2017/10/6 21:33
  */
 public class MemcacheHelper {
+
+    private static final Logger LOG = Logger.getLogger(MemcacheHelper.class);
+
 }
