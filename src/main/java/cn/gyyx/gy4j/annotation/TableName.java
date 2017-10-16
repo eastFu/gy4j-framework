9ed18f206cc0213296fadc9697002f80bb5d4cec
eastFu <492510931@qq.com>
2017-10-16 17:29:59 +0800

    0.0.1

---
+++ src/main/java/cn/gyyx/gy4j/annotation/TableName.java
@@ -0,0 +1,15 @@
+package cn.gyyx.gy4j.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @Author : east.Fu
+ * @Description :
+ * @Date : Created in  2017/10/16 17:22
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface TableName {
+    String name();
+}
