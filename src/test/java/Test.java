57aa99af1ff571fa348936073fd651648e145382
eastFu <492510931@qq.com>
2017-10-17 18:07:48 +0800

    0.0.1

---
+++ src/test/java/Test.java
@@ -0,0 +1,26 @@
+import cn.gyyx.test.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Author : east.Fu
+ * @Description :
+ * @Date : Created in  2017/10/17 15:04
+ */
+public class Test {
+
+    ApplicationContext app =null;
+
+    @Before
+    public  void init(){
+        app =new ClassPathXmlApplicationContext(new String[] { "classpath:springContext-dataSource.xml"});
+    }
+
+    @org.junit.Test
+    public void test(){
+        UserService service=app.getBean(UserService.class);
+        service.query();
+    }
+}
