e44844f6264f11a3a237286a9102901af30b7199
eastFu <492510931@qq.com>
2017-10-17 18:06:47 +0800

    0.0.1

---
+++ src/main/java/cn/gyyx/test/UserMapper.java
@@ -0,0 +1,13 @@
+package cn.gyyx.test;
+
+import cn.gyyx.gy4j.model.RbacUser;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * @Author : east.Fu
+ * @Description :
+ * @Date : Created in  2017/10/17 14:49
+ */
+public interface UserMapper extends BaseMapper<RbacUser> {
+
+}
