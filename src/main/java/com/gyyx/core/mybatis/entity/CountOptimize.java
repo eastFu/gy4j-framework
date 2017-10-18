777a0063c025a498f840e0cd95e0ddd4dc94d07a
eastFu <492510931@qq.com>
2017-10-18 17:13:14 +0800

    0.0.1

---
+++ src/main/java/com/gyyx/core/mybatis/entity/CountOptimize.java
@@ -0,0 +1,34 @@
+package com.gyyx.core.mybatis.entity;
+
+public class CountOptimize {
+
+    /**
+     * 是否排序
+     */
+    private boolean orderBy = true;
+    /**
+     * 优化后计算Count的SQL
+     */
+    private String countSQL;
+
+    public static CountOptimize newInstance() {
+        return new CountOptimize();
+    }
+
+    public boolean isOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(boolean orderBy) {
+        this.orderBy = orderBy;
+    }
+
+    public String getCountSQL() {
+        return countSQL;
+    }
+
+    public void setCountSQL(String countSQL) {
+        this.countSQL = countSQL;
+    }
+
+}
---
+++ src/main/java/com/gyyx/core/mybatis/toolkit/PluginUtils.java
@@ -0,0 +1,24 @@
+package com.gyyx.core.mybatis.toolkit;
+
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
+
+import java.lang.reflect.Proxy;
+
+public final class PluginUtils {
+
+    private PluginUtils() {
+    }
+
+    /**
+     * 获得真正的处理对象,可能多层代理.
+     */
+    public static Object realTarget(Object target) {
+        if (Proxy.isProxyClass(target.getClass())) {
+            MetaObject metaObject = SystemMetaObject.forObject(target);
+            return realTarget(metaObject.getValue("h.target"));
+        }
+        return target;
+    }
+
+}
