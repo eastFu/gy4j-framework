6b43ea26ad2f6a52f5bcda1680ccad41d450ea5a
eastFu <492510931@qq.com>
2017-10-24 12:27:27 +0800

    gy4j framework v0.0.1

---
+++ gy4j-rbac/src/main/java/cn/gyyx/rbac/mapper/RbacRoleMapper.java
@@ -0,0 +1,17 @@
+package cn.gyyx.rbac.mapper;
+
+import cn.gyyx.rbac.entity.RbacRole;
+import cn.gyyx.framework.mybatis.mapper.BaseMapper;
+import cn.gyyx.framework.mybatis.annotations.MyBatisDao
+/**
+ * <p>
+  * 系统角色表 Mapper 接口
+ * </p>
+ *
+ * @author east.Fu
+ * @since 2017-10-24
+ */
+@MyBatisDao
+public interface RbacRoleMapper extends BaseMapper<RbacRole> {
+
+}
---
+++ gy4j-rbac/src/main/java/cn/gyyx/rbac/service/IRbacIconclsService.java
@@ -0,0 +1,13 @@
+package cn.gyyx.rbac.service;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author east.Fu
+ * @since 2017-10-24
+ */
+public interface IRbacIconclsService{
+	
+}
