dd269ddcba7199708fd3db52c0f6ace7e8b6613d
eastFu <492510931@qq.com>
2017-10-29 15:15:35 +0800

    gy4j framework v0.0.1

--- gy4j-core/src/main/java/cn/gyyx/framework/exception/BusinessException.java
+++ gy4j-core/src/main/java/cn/gyyx/framework/exception/BusinessException.java
@@ -1,8 +1,14 @@
 package cn.gyyx.framework.exception;
 
 public class BusinessException extends RuntimeException{
-    
+
     public BusinessException(String message){
         super(message);
+    }
+    public BusinessException(Throwable throwable){
+        super(throwable);
+    }
+    public BusinessException(String message,Throwable throwable){
+        super(message,throwable);
     }
 }
