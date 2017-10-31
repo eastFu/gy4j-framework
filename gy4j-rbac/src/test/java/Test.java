1f918fb54699cceecce52f877823202a5e5c6ef2
eastFu <492510931@qq.com>
2017-10-31 18:03:37 +0800

    gy4j framework v0.0.1

---
+++ gy4j-rbac/src/test/java/Test.java
@@ -0,0 +1,23 @@
+/**
+ * @Author : east.Fu
+ * @Description :
+ * @Date : Created in  2017/10/30 16:24
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * test
+ * @author
+ * @create 2017-10-30 16:24
+ **/
+public class Test {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Test.class);
+
+    public static void main(String[] args) {
+//        ApplicationContext spring = new ClassPathXmlApplicationContext(new String[] {"spring.xml","srping-mvc.xml"});
+        LOGGER.info("sdfsdf");
+    }
+}
