626df88b97c29ff1360b86be7e0d58bc75042b9c
eastFu <492510931@qq.com>
2017-11-13 11:06:53 +0800

    gy4j-generator update

--- gy4j-generator/src/test/java/GeneratorTest.java
+++ gy4j-generator/src/test/java/GeneratorTest.java
@@ -8,13 +8,13 @@ import cn.gyyx.framework.generator.SimpleGenerator;
 public class GeneratorTest {
 
     public static void main(String[] args) {
-        String moduleName="appstore";
+        String moduleName="test";
         String userName="root";
         String pwd="root";
-        String url="jdbc:mysql://192.168.10.36:3306/app_store?characterEncoding=utf8";
-        String author="east.Fu";
-        String outPath="E://app-store";
-        
+        String url="jdbc:mysql://127.0.0.1:3306/test?characterEncoding=utf8";
+        String author="east";
+        String outPath="E://test";
+
         SimpleGenerator generator=new SimpleGenerator(moduleName,userName,pwd,url,outPath,author);
         generator.run();
     }
